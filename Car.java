6203702fed1014346438cb13fd6016c68900c1e8
Ramesh Timsina <rtimsina@c4c2c1.42berlin.de>
2023-10-15 17:09:27 +0200

    some try

---
+++ Car.java
@@ -0,0 +1,7 @@
+public class Car {
+	public String Kennzeichen;
+
+	public Car(String Kennzeichen){
+		this.Kennzeichen = Kennzeichen;
+	}
+}
---
+++ ParkingLot.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class ParkingLot {
+	public List<Level> levels;
+
+	public ParkingLot(List<Level> levels){
+		this.levels = levels;
+	}
+
+	public boolean park(Car car){
+		for (Level level:levels){
+			if(level.park(car)){
+				return true;
+			}else{
+				continue;
+			}
+		}
+		return false;
+	}
+}
